1da803c1738c4b993ee5e17bdecfda39fb0c4859
dulda280 <Sebber280@live.dk>
2022-04-10 18:41:08 +0200

    added more lyrics and started Jupyter notebook for RNN

--- Wordsmith.py
+++ Wordsmith.py
@@ -1,6 +1,7 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
 import nltk
 import os
+import phonetics as ph
 
 #
 # with open(f"{cwd}\inspiration\{dataroot[0]}").read().replace('\n', '. ') as file:
@@ -8,10 +9,15 @@ import os
 cwd = os.getcwd()
 dataroot = os.listdir(cwd + "\inspiration")
 
+sentData = []
 
 for i in range(0, len(dataroot)):
     data2 = open(f"{cwd}\inspiration\{dataroot[i]}").read().replace('\n', '. ')
-    print(data2)
-    print(sent_tokenize(data2))
+    sentData.append(sent_tokenize(data2))
+
+print(sentData[2])
+print("test: ", ph.dmetaphone("Take"))
+print("test2: ", ph.dmetaphone("Bake"))
+
 
 
